c9fdb6b2f38fc2236ec20884659fdb0f305faf37
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2020-07-03 13:08:19 -0400

    Changed Masscan output format (-oL); Updated scan_handler and parser to accommodate

--- pyscan_funcs.py
+++ pyscan_funcs.py
@@ -1,7 +1,7 @@
-import ast
 import datetime
 from os import mkdir
 import subprocess as sp
+from typing import Tuple
 
 
 def direct_create():
@@ -11,36 +11,39 @@ def direct_create():
         pass
 
 
-def masscan_parser(json_output: str):
+def masscan_parser(masscan_output: str) -> Tuple[str, str]:
     """
-    Takes a file produced from Masscan's -oJ option and returns two lists - System addresses and Ports
-    :param json_output:
+    Takes a file produced from Masscan's -oL option and returns two strings
+    :param masscan_output:
     :return addresses, ports:
     """
 
-    with open(json_output) as f:
-        temp_data = ast.literal_eval(f.read())
+    with open(masscan_output) as f:
+        temp_data = f.readlines()
 
-    addresses = ",".join(list(set(host['ip'] for host in temp_data)))
+    addresses = ",".join(list(set(host.split()[3] for host in temp_data)))
 
-    ports = ",".join(list(set(host['ports'][0]['port'] for host in temp_data)))
+    ports = ",".join(list(set(host.split()[2] for host in temp_data)))
 
     return addresses, ports
 
 
 def scan_handler(ranges: dict, timestamp: datetime):
     """
-
+    Initiates a Masscan, then feeds the output back into Nmap for a more detailed scan of online hosts/listening ports
     :param ranges:
     :param timestamp:
     :return:
     """
     for site, subnet in ranges.items():
         # First we'll launch a masscan against our specified ranges
-        sp.run(f"masscan {subnet} --ports 0-65535 --rate 10000 -oJ {site}_mass_{timestamp}.json", shell=True)
+        # -p1-65535,U:1-65535 -> All ports, TCP and UDP alike
+        # --rate 1000 -> 1k packets/sec; Can go faster, but speed seems to scale inversely with accuracy
+        # -oL -> Exports results in plaintext
+        sp.run(f"masscan {subnet} -p1-65535,U:1-65535 --rate 10000 -oL {site}_mass_{timestamp}", shell=True)
 
-        # Load, clean, and format our masscan json for nmap
-        addresses, ports = masscan_parser(f"{site}_mass_{timestamp}.json")
+        # Load, clean, and format our masscan txt for nmap
+        addresses, ports = masscan_parser(f"{site}_mass_{timestamp}")
 
         # Launch an Nmap scan for each set of data
         sp.run(f"nmap -sS -sU {addresses} -p {ports} -oX {site}_{timestamp}.xml", shell=True)
